7f269575fa2734901e91e5919b48bad499ba7ccd
PhemeloMotsamai <phemelojmotsamai@gmail.com>
2021-05-04 02:25:15 +0200

    Main method has been added sysoutprintln code

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@
 public class Test {
     public static void main(String [] args){
 
-        System.out.println("hello world");
+        System.out.println("hello mzanzi");
+        System.out.println("I am here with git");
     }
 }
